cedeab955f18184a41f350e5f5d3674100ecf095
ErykaMML <eryka.lopes@sptech.school>
2023-04-04 21:32:46 -0300

    Código do looca para listar as informações

---
+++ site/public/sprint-testee/src/main/java/org/example/CodigoLooca.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.Disco;
+import com.github.britooo.looca.api.group.discos.DiscoGrupo;
+import com.github.britooo.looca.api.group.memoria.Memoria;
+import com.github.britooo.looca.api.group.processador.Processador;
+import com.github.britooo.looca.api.group.rede.Rede;
+import com.github.britooo.looca.api.group.sistema.Sistema;
+
+import java.util.List;
+
+public class CodigoLooca {
+    public static void main(String[] args) {
+        Looca looca = new Looca();
+        Sistema sistema = looca.getSistema();
+        Memoria memoria = looca.getMemoria();
+        Processador cpu = looca.getProcessador();
+        Rede rede = looca.getRede();
+
+        System.out.println("---------------------Sistema-----------------------");
+        System.out.println(sistema);
+        System.out.println("---------------------Memória-----------------------");
+        System.out.println(memoria);
+        System.out.println("---------------------CPU-----------------------");
+        System.out.println(cpu);
+        System.out.println("---------------------Rede-----------------------");
+        System.out.println(rede);
+
+        System.out.println("---------------------Disco-----------------------");
+    // Listando as informações de disco (nome, modelo, serial, tamanho,
+        // leitura, bytes de leitura, escritas, bytes de escritas, tamanho atual da fila, tempo de transferência)
+
+        DiscoGrupo grupoDeDiscos = looca.getGrupoDeDiscos();
+        List<Disco> discos = grupoDeDiscos.getDiscos();
+        for (Disco disco : discos) {
+            System.out.println(disco);
+        }
+
+
+
+
+    }
+}
